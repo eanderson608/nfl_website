83a666fd8f9f5019670dbee6251ab0f007b847f6
eanderson608 <eanderson608@gmail.com>
2016-10-15 18:33:30 -0500

    seperated angular modules and controllers into different files

---
+++ js/controllers/MainController.js
@@ -0,0 +1,6 @@
+app.controller('MainController', function($scope, $http) {
+	$http.get("http://ec2-54-244-135-126.us-west-2.compute.amazonaws.com/gettabledata.php")
+		.then(function(response) {
+			$scope.records = response.data.records;
+		});
+});
