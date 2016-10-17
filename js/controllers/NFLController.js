047b82f3f25de0fa008a9dbd02e27d53dfaaf7f4
eanderson608 <eanderson608@gmail.com>
2016-10-17 13:20:07 -0500

    added website description

--- js/controllers/NFLController.js
+++ js/controllers/NFLController.js
@@ -2,5 +2,8 @@ app.controller('NFLController', function($scope, $http) {
 	$http.get("http://ec2-54-214-112-22.us-west-2.compute.amazonaws.com/testdata.php")
 		.then(function(response) {
 			$scope.tables = response.data;
+		}, function(error) {
+			$scope.status = error.status;
+			console.error(error);
 		});
 });
