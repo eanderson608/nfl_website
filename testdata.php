5acdf37c7448b084498da3fef88b14bdb0da4af2
eanderson608 <eanderson608@gmail.com>
2016-10-17 09:22:06 -0500

    added testdata.php for sample data

--- testdata.php
+++ testdata.php
@@ -1,26 +1,26 @@
 <?php
 
-  header("Access-Control-Allow-Origin: *");
-  header("Content-Type: application/json; charset=UTF-8");
+	header("Access-Control-Allow-Origin: *");
+	header("Content-Type: application/json; charset=UTF-8");
 
-  echo('{
-    "game1": [
-            {"Day":"Mon", "Line":"-6", "Consensus": "60"},
-            {"Day":"Tue", "Line":"-6", "Consensus": "59"},
-            {"Day":"Wed", "Line":"-6.5", "Consensus": "55"},
-            {"Day":"Thu", "Line":"-7", "Consensus": "51"}
-            ],
-    "game2": [
-            {"Day":"Mon", "Line":"14", "Consensus": "45"},
-            {"Day":"Tue", "Line":"14", "Consensus": "46"},
-            {"Day":"Wed", "Line":"14.5", "Consensus": "50"},
-            {"Day":"Thu", "Line":"14.5", "Consensus": "51"}
-            ],
-    "game3": [
-            {"Day":"Mon", "Line":"-10", "Consensus": "25"},
-            {"Day":"Tue", "Line":"-10.5", "Consensus": "24"},
-            {"Day":"Wed", "Line":"-11", "Consensus": "25"},
-            {"Day":"Thu", "Line":"-11", "Consensus": "26"}
-            ]
-    }');
+	echo('{
+		"game1": [
+			{"Day":"Mon", "Line":"-6", "Consensus": "60"},
+			{"Day":"Tue", "Line":"-6", "Consensus": "59"},
+			{"Day":"Wed", "Line":"-6.5", "Consensus": "55"},
+			{"Day":"Thu", "Line":"-7", "Consensus": "51"}
+			],
+		"game2": [
+			{"Day":"Mon", "Line":"14", "Consensus": "45"},
+			{"Day":"Tue", "Line":"14", "Consensus": "46"},
+			{"Day":"Wed", "Line":"14.5", "Consensus": "50"},
+			{"Day":"Thu", "Line":"14.5", "Consensus": "51"}
+			],
+		"game3": [
+			{"Day":"Mon", "Line":"-10", "Consensus": "25"},
+			{"Day":"Tue", "Line":"-10.5", "Consensus": "24"},
+			{"Day":"Wed", "Line":"-11", "Consensus": "25"},
+			{"Day":"Thu", "Line":"-11", "Consensus": "26"}
+			]
+		}');
 ?>
